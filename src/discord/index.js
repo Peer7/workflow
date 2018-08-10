8ac899c638f1a2279b7747d76365a4c4135ff2b5
Puthranps <puthranpreethesh@gmail.com>
2018-08-10 01:22:10 -0400

    Initial bot commit

--- src/discord/index.js
+++ src/discord/index.js
@@ -1,1 +1,31 @@
-console.log('ere')
+const discord = require('discord.js');
+const client = new discord.Client();
+const { key, secret, prefix } = require('../config/discord_api.json');
+
+
+client.on('ready', () => {
+  console.log('operating');
+});
+
+client.on('guildMemberAdd', async member => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+​
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find('name', 'general').send(`Welcome to the server: ${userlist}`);
+    newUsers[guild.id].clear();
+  }
+});
+​
+client.on('guildMemberRemove', async member => {
+  const guild = member.guild;
+  if (newUsers[guild.id].has(member.id)) newUsers.delete(member.id);
+});
+
+client.on('message', async message => {
+
+});
+
+client.login(secret);
