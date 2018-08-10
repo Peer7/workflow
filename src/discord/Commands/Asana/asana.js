1dd04f2ee32613c8f17e07ce63a7e64e70210200
Puthranps <puthranpreethesh@gmail.com>
2018-08-10 15:07:49 -0400

    Added basic asana command

--- src/discord/Commands/Asana/asana.js
+++ src/discord/Commands/Asana/asana.js
@@ -1,3 +1,49 @@
+const asana = require('asana');
+const { clientID, clientSecret } = require('../../../../config/asana_config.json');
+
 module.export = class Asana {
-  
+  constructor(...args) {
+    this.client = asana.Client.create().useAccessToken(clientSecret);
+    this.taskHandler.bind(this);
+    this.getTasks.bind(this);
+  }
+
+  this.client.users.me().then( me => {
+    console.log(me);
+  });
+
+  taskHandler(...args) {
+    switch(..args) {
+      default:
+        return "Invalid command"
+    }
+  }
+
+  getTasks() {
+    client.users.me()
+    .then(user => {
+      const userId = user.id;
+      const workspaceId = user.workspaces[0].id;
+      return client.tasks.findAll({
+        assignee: userId,
+        workspace: workspaceId,
+        completed_since: 'now',
+        opt_fields: 'id,name,assignee_status,completed'
+      });
+    })
+    .then(response => { return response.data; })
+    .filter(task => {
+      return task.assignee_status === 'today' ||
+        task.assignee_status === 'new';
+    })
+    .then(list => {
+      console.log(util.inspect(list, {
+        colors: true,
+        depth: null
+      }));
+    })
+    .catch(e => {
+      console.log(e);
+    });
+  }
 }
